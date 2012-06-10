d179b3c5c026d88dfb5ca8eb8f7c66256de7bf61
Michael <michael@michael-Latop.(none)>
2012-06-09 20:11:22 -0400

    -Made maptoodom stupider (and maybe right).

--- stark_frames/src/mapToOdom.cpp
+++ stark_frames/src/mapToOdom.cpp
@@ -50,18 +50,6 @@ void poseCallback(const geometry_msgs::Pose2D::ConstPtr& msg)
         ROS_ERROR("%s",ex.what());
     }
     
-    /*tf::Quaternion MBR;
-    MBR.setEuler(msg->theta, 0, 0);
-    
-    tf::Transform MB(MBR, tf::Point(msg->x - gpsToMap.getOrigin().x(), msg->y - gpsToMap.getOrigin().y(), 0.0));
-    
-    tf::Transform OBRI(odomToBase.getRotation().inverse(), tf::Point(0, 0, 0));
-    
-    tf::Transform OBTI(tf::Quaternion(0, 0, 0, 1), 
-                       odomToBase.getOrigin() * -1.0);
-    
-    tfBroadcaster->sendTransform(tf::StampedTransform(OBTI * OBRI * MB, now, "/map", "/odom"));*/
-    
     tf::Vector3 pos;
     pos.setX(msg->x - gpsToMap.getOrigin().x());
     pos.setY(msg->y - gpsToMap.getOrigin().y());
@@ -69,8 +57,8 @@ void poseCallback(const geometry_msgs::Pose2D::ConstPtr& msg)
     tf::Quaternion quat;
     quat.setRPY(0, 0, msg->theta);
     
-    quat = quat * odomToBase.getRotation().inverse();
-    pos = pos - odomToBase.getOrigin();
+    /*quat = quat * odomToBase.getRotation().inverse();
+    pos = pos - odomToBase.getOrigin();*/
     
     tf::Transform mapToOdom(quat, pos);
     
